12e749354baf474f4a5c7704d492b4f05bc0649a
nikthechampiongr <32041239+nikthechampiongr@users.noreply.github.com>
2024-09-08 07:28:43 +0000

    Add completions to addobjective and localize it (#30456)
    * Add completions to addobjective and localise it
    
    * Cleanup
    
    * Fix
    
    * Make a manager to handle subscribtion completion options
    
    This is so we can unsubscribe to prototype reloads properly
    
    * Convert the manager into a system
    
    * Move the system into the systems folder
    
    I forgor
    
    * Merge CompletionsSystem into ObjectivesSystem

--- Content.Server/Entry/EntryPoint.cs
+++ Content.Server/Entry/EntryPoint.cs
@@ -68,6 +68,7 @@ using Content.Server.IoC;
 using Content.Server.Players.JobWhitelist;
 using Content.Server.Maps;
 using Content.Server.NodeContainer.NodeGroups;
+using Content.Server.Objectives;
 using Content.Server.Players;
 using Content.Server.Players.JobWhitelist;
 using Content.Server.Players.PlayTimeTracking;
