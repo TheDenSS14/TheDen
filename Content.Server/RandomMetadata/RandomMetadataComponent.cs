1d20c58dbb26d795f5fa91f77d2a9352816d86b6
Tayrtahn <tayrtahn@gmail.com>
2025-04-06 14:12:39 -0400

    Names

--- Content.Server/RandomMetadata/RandomMetadataComponent.cs
+++ Content.Server/RandomMetadata/RandomMetadataComponent.cs
@@ -1,4 +1,7 @@
-﻿namespace Content.Server.RandomMetadata;
+using Content.Shared.Dataset;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server.RandomMetadata;
 
 /// <summary>
 ///     Randomizes the description and/or the name for an entity by creating it from list of dataset prototypes or strings.
@@ -12,9 +15,17 @@ public sealed partial class RandomMetadataComponent : Component
     [DataField("nameSegments")]
     public List<string>? NameSegments;
 
-    [DataField("nameSeparator")]
-    public string NameSeparator = " ";
+    /// <summary>
+    /// LocId of the formatting string to use to assemble the <see cref="NameSegments"/> into the entity's name.
+    /// Segments will be passed to the localization system with this string as variables named $part0, $part1, $part2, etc.
+    /// </summary>
+    [DataField]
+    public LocId NameFormat = "random-metadata-name-format-default";
 
-    [DataField("descriptionSeparator")]
-    public string DescriptionSeparator = " ";
+    /// <summary>
+    /// LocId of the formatting string to use to assemble the <see cref="DescriptionSegments"/> into the entity's description.
+    /// Segments will be passed to the localization system with this string as variables named $part0, $part1, $part2, etc.
+    /// </summary>
+    [DataField]
+    public LocId DescriptionFormat = "random-metadata-description-format-default";
 }
