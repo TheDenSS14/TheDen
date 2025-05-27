RebelledCombine/Zona-14

[Part of #32893] Localize humanoid species dataset names (#33395)
* Localize diona names

* Localize moth names

* Localize mushman names

* Localize reptilian names

* Localize skeleton names

* Upstream diona names

* names-moth-male/female-first-dataset -> names-moth-first-male/female-dataset

* Correct prototype ids to follow naming conventions

* NamesSkeletonFirst -> NamesSkeleton

* Combine moth_first_female.yml, moth_first_male.yml and moth_last.yml

* Forgot about skeleton prototype

* Upstream names

* Update Resources/Locale/en-US/datasets/names/diona_last.ftl

* Update Resources/Locale/en-US/datasets/names/diona_last.ftl

* keep first name for skeleton

---------

Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>

Resources/Locale/en-US/datasets/names/skeleton_first.ftl
@@ -1,0 +1,27 @@
+names-skeleton-first-dataset-1 = Sternum
+names-skeleton-first-dataset-2 = Ribs
+names-skeleton-first-dataset-3 = Vertebrae
+names-skeleton-first-dataset-4 = Sacrum
+names-skeleton-first-dataset-5 = Mandible
+names-skeleton-first-dataset-6 = Clavicle
+names-skeleton-first-dataset-7 = Scapula
+names-skeleton-first-dataset-8 = Humerus
+names-skeleton-first-dataset-9 = Radius
+names-skeleton-first-dataset-10 = Ulna
+names-skeleton-first-dataset-11 = Carpals
+names-skeleton-first-dataset-12 = Phalanges
+names-skeleton-first-dataset-13 = Pelvis
+names-skeleton-first-dataset-14 = Femur
+names-skeleton-first-dataset-15 = Tibia
+names-skeleton-first-dataset-16 = Fibula
+names-skeleton-first-dataset-17 = Marrow
+names-skeleton-first-dataset-18 = Tarsals
+names-skeleton-first-dataset-19 = Patella
+names-skeleton-first-dataset-20 = Tailbone
+names-skeleton-first-dataset-21 = Boner
+names-skeleton-first-dataset-22 = Rib
+names-skeleton-first-dataset-23 = Hyoid
+names-skeleton-first-dataset-24 = Coccyx
+names-skeleton-first-dataset-25 = Tarsus
+names-skeleton-first-dataset-26 = Lacrimal
+names-skeleton-first-dataset-27 = Bone

TheDenSS14/TheDen

Resources/Locale/en-US/datasets/names/skeleton_first.ftl
@@ -1,0 +1,28 @@
+names-skeleton-first-dataset-1 = Sternum
+names-skeleton-first-dataset-2 = Ribs
+names-skeleton-first-dataset-3 = Vertebrae
+names-skeleton-first-dataset-4 = Sacrum
+names-skeleton-first-dataset-5 = Mandible
+names-skeleton-first-dataset-6 = Clavicle
+names-skeleton-first-dataset-7 = Scapula
+names-skeleton-first-dataset-8 = Humerus
+names-skeleton-first-dataset-9 = Radius
+names-skeleton-first-dataset-10 = Ulna
+names-skeleton-first-dataset-11 = Carpals
+names-skeleton-first-dataset-12 = Phalanges
+names-skeleton-first-dataset-13 = Pelvis
+names-skeleton-first-dataset-14 = Femur
+names-skeleton-first-dataset-15 = Tibia
+names-skeleton-first-dataset-16 = Fibula
+names-skeleton-first-dataset-17 = Marrow
+names-skeleton-first-dataset-18 = Tarsals
+names-skeleton-first-dataset-19 = Patella
+names-skeleton-first-dataset-20 = Tailbone
+# DeltaV - no Boner
+names-skeleton-first-dataset-21 = Bone
+names-skeleton-first-dataset-22 = Rib
+names-skeleton-first-dataset-23 = Hyoid
+names-skeleton-first-dataset-24 = Coccyx
+names-skeleton-first-dataset-25 = Tarsus
+names-skeleton-first-dataset-26 = Lacrimal
+names-skeleton-first-dataset-27 = Bone