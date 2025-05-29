3fd83311921a2cd19d6daf4f098bfe4183119869
sleepyyapril <flyingkarii@gmail.com>
2025-05-29 15:07:46 -0300

    !

--- Content.Server/PresetPicker/PresetPickerPrototype.cs
+++ Content.Server/PresetPicker/PresetPickerPrototype.cs
@@ -1,19 +1,17 @@
 using Content.Server.GameTicking.Presets;
 using Robust.Shared.Prototypes;
 
-
 namespace Content.Server.PresetPicker;
-
 
 /// <summary>
 /// This is a prototype for picking a prototype for use in presets.
 /// </summary>
-[Prototype("presetPicker")]
+[Prototype]
 public sealed partial class PresetPickerPrototype : IPrototype
 {
     /// <inheritdoc/>
     [IdDataField]
-    public string ID { get; } = default!;
+    public string ID { get; private set; } = default!;
 
     /// <summary>
     ///     Choose from a list of presets.
