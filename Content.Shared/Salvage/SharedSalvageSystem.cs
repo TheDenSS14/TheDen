7fa068ed32cd0c0b5fa9be5751b836dc0bd33761
BlitzTheSquishy <73762869+BlitzTheSquishy@users.noreply.github.com>
2025-05-30 09:34:45 +1000

    missed some C#

--- Content.Shared/Salvage/SharedSalvageSystem.cs
+++ Content.Shared/Salvage/SharedSalvageSystem.cs
@@ -26,7 +26,7 @@ public abstract partial class SharedSalvageSystem : EntitySystem
     [ValidatePrototypeId<SalvageLootPrototype>]
     public const string ExpeditionsLootProto = "SalvageLoot";
 
-    public static string GetFTLName(DatasetPrototype dataset, int seed)
+    public static string GetFTLName(LocalizedDatasetPrototype dataset, int seed)
     {
         var random = new System.Random(seed);
         return $"{dataset.Values[random.Next(dataset.Values.Count)]}-{random.Next(10, 100)}-{(char) (65 + random.Next(26))}";
