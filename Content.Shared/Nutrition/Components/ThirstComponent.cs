f15ee74a07e1be7673cd5715cb7808e4f8488db2
sleepyyapril <flyingkarii@gmail.com>
2025-06-04 00:43:05 -0300

    Less

--- Content.Shared/Nutrition/Components/ThirstComponent.cs
+++ Content.Shared/Nutrition/Components/ThirstComponent.cs
@@ -14,7 +14,7 @@ public sealed partial class ThirstComponent : Component
     [ViewVariables(VVAccess.ReadWrite)]
     [DataField("baseDecayRate")]
     [AutoNetworkedField]
-    public float BaseDecayRate = 0.025f;
+    public float BaseDecayRate = 0.008f;
 
     [ViewVariables(VVAccess.ReadWrite)]
     [AutoNetworkedField]
