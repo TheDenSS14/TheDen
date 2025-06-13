135ef74246eb4aebaa47eb2174f0a05b616f65e6
Cam <Nop>
2025-06-13 09:37:54 +0100

    Quick mockup

---
+++ Content.Client/UserInterface/Systems/Chat/Controls/Denu/DenuButton.cs
@@ -0,0 +1,89 @@
+﻿using System.Numerics;
+using Content.Client.Resources;
+using Content.Client.UserInterface.Systems.Chat.Controls.Denu;
+using Robust.Client.ResourceManagement;
+using Robust.Client.UserInterface.Controls;
+
+
+namespace Content.Client.UserInterface.Systems.Chat.Controls.Denu;
+
+
+public sealed class DenuButton : ChatPopupButton<DenuPopup>
+{
+    public static readonly Color ColorNormal = Color.FromHex("#7b7e9e");
+    public static readonly Color ColorHovered = Color.FromHex("#9699bb");
+    public static readonly Color ColorPressed = Color.FromHex("#789B8C");
+
+    private readonly TextureRect? _textureRect;
+    private readonly ChatUIController _chatUIController;
+
+    private const int FilterDropdownOffset = 120;
+
+    public DenuButton()
+    {
+        _chatUIController = UserInterfaceManager.GetUIController<ChatUIController>();
+        var filterTexture = IoCManager.Resolve<IResourceCache>()
+            .GetTexture("/Textures/Interface/Nano/filter.svg.96dpi.png");
+
+        AddChild(
+            (_textureRect = new TextureRect
+            {
+                Texture = filterTexture,
+                HorizontalAlignment = HAlignment.Center,
+                VerticalAlignment = VAlignment.Center
+            })
+        );
+    }
+
+    protected override UIBox2 GetPopupPosition()
+    {
+        var globalPos = GlobalPosition;
+        var (minX, minY) = Popup.MinSize;
+        return UIBox2.FromDimensions(
+            globalPos - new Vector2(FilterDropdownOffset, 0),
+            new Vector2(Math.Max(minX, Popup.MinWidth), minY));
+    }
+
+    private void UpdateChildColors()
+    {
+        if (_textureRect == null)
+            return;
+        switch (DrawMode)
+        {
+            case DrawModeEnum.Normal:
+                _textureRect.ModulateSelfOverride = ColorNormal;
+                break;
+
+            case DrawModeEnum.Pressed:
+                _textureRect.ModulateSelfOverride = ColorPressed;
+                break;
+
+            case DrawModeEnum.Hover:
+                _textureRect.ModulateSelfOverride = ColorHovered;
+                break;
+
+            case DrawModeEnum.Disabled:
+                break;
+        }
+    }
+
+    protected override void DrawModeChanged()
+    {
+        base.DrawModeChanged();
+        UpdateChildColors();
+    }
+
+    protected override void StylePropertiesChanged()
+    {
+        base.StylePropertiesChanged();
+        UpdateChildColors();
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+
+        if (!disposing)
+            return;
+    }
+}
---
+++ Content.Client/UserInterface/Systems/Chat/Controls/Denu/DenuUIController.cs
@@ -0,0 +1,24 @@
+﻿using Content.Client.Chat.TypingIndicator;
+using OpenToolkit.GraphicsLibraryFramework;
+using Robust.Client.UserInterface;
+using Robust.Client.UserInterface.Controllers;
+using Robust.Shared.Timing;
+using Serilog;
+
+namespace Content.Client.UserInterface.Systems.Chat.Controls.Denu;
+
+public sealed partial class DenuUIController : UIController
+{
+    [UISystemDependency] private readonly TypingIndicatorSystem? _typingIndicator = default;
+    
+    public void TypingToggleUpdate()
+    {
+        _typingIndicator!.ClientChangedChatText();
+    }
+
+    public void TypingToggleDisabled()
+    {
+        _typingIndicator!.ClientSubmittedChatText();
+    }
+
+}
