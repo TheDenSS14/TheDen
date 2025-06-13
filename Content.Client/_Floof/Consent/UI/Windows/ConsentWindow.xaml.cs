47d811860b64b10c5ddbd83c04137577a0b28fa3
Sol <ewokgotswag222@gmail.com>
2025-06-12 21:42:07 -0400

    xcard

--- Content.Client/_Floof/Consent/UI/Windows/ConsentWindow.xaml.cs
+++ Content.Client/_Floof/Consent/UI/Windows/ConsentWindow.xaml.cs
@@ -13,15 +13,21 @@ using Robust.Client.UserInterface.XAML;
 using Robust.Shared.Configuration;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
-
+using System.Linq;
+using Content.Shared._AS.Consent;
+using Robust.Client.Player;
+using Robust.Client.UserInterface;
+using Robust.Shared.Player;
 
 namespace Content.Client._Floof.Consent.UI.Windows;
 
 [GenerateTypedNameReferences]
 public sealed partial class ConsentWindow : FancyWindow
 {
+    [Dependency] private readonly SharedConsentCardSystem _card = default!;
     [Dependency] private readonly IClientConsentManager _consentManager = default!;
     [Dependency] private readonly IConfigurationManager _configManager = default!;
+    [Dependency] private readonly ISharedPlayerManager _player = default!;
     [Dependency] private readonly IPrototypeManager _protoManager = default!;
     private readonly List<EntryState> _entries = new();
 
@@ -42,14 +48,13 @@ public sealed partial class ConsentWindow : FancyWindow
 
         ConsentFreetext.Placeholder = new Rope.Leaf(Loc.GetString("consent-window-freetext-placeholder"));
         ConsentFreetext.OnTextChanged += _ => UnsavedChanges();
-    }
 
-    protected override void Dispose(bool disposing)
-    {
-        base.Dispose(disposing);
-
-        if (disposing)
-            _consentManager.OnServerDataLoaded -= UpdateUi;
+        // Aurora - Add consent cards
+        XCard.OnPressed += _ =>
+        {
+            if (_player.LocalSession is {} player)
+                _card.RaiseConsentCard(player.UserId, "XCard");
+        };
     }
 
     private PlayerConsentSettings GetSettings()
