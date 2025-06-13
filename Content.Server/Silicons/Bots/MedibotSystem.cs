a5b54afdb72c8cc8626194c5ba066dcafc2b4c92
portfiend <109661617+portfiend@users.noreply.github.com>
2025-06-13 13:01:31 -0400

    fix medibot emag feedback

--- Content.Server/Silicons/Bots/MedibotSystem.cs
+++ Content.Server/Silicons/Bots/MedibotSystem.cs
@@ -6,18 +6,13 @@ using Content.Server.Popups;
 using Content.Shared._DEN.Silicons.Bots.Components;
 using Content.Shared.Chat;
 using Content.Shared.Chemistry.Components;
-using Content.Shared.Chemistry.Components.SolutionManager;
 using Content.Shared.Chemistry.EntitySystems;
 using Content.Shared.Chemistry.Hypospray.Events;
 using Content.Shared.Damage;
 using Content.Shared.DoAfter;
 using Content.Shared.Emag.Components;
-using Content.Shared.Emag.Systems;
-using Content.Shared.IdentityManagement;
 using Content.Shared.Mobs.Components;
-using Content.Shared.Silicons.Borgs.Components;
 using Content.Shared.Silicons.Bots;
-using Robust.Shared.Audio.Systems;
 using Robust.Shared.Containers;
 
 namespace Content.Server.Silicons.Bots;
@@ -25,7 +20,6 @@ namespace Content.Server.Silicons.Bots;
 public sealed class MedibotSystem : SharedMedibotSystem
 {
     [Dependency] private readonly ActionsSystem _actions = default!;
-    [Dependency] private readonly SharedAudioSystem _audio = default!;
     [Dependency] private readonly ChatSystem _chat = default!;
     [Dependency] private readonly SharedContainerSystem _container = default!;
     [Dependency] private readonly HypospraySystem _hypospray = default!;
@@ -37,7 +31,6 @@ public sealed class MedibotSystem : SharedMedibotSystem
         base.Initialize();
 
         SubscribeLocalEvent<MedibotComponent, ComponentStartup>(OnMedibotStartup);
-        SubscribeLocalEvent<EmaggableMedibotComponent, GotEmaggedEvent>(OnEmagged);
         SubscribeLocalEvent<MedibotInjectorComponent, HyposprayDoAfterEvent>(AfterInjected,
             after: [typeof(HypospraySystem)]);
         SubscribeLocalEvent<MedibotInjectTargetEvent>(OnInjectTarget);
@@ -54,19 +47,6 @@ public sealed class MedibotSystem : SharedMedibotSystem
             injectorComp.Medibot = uid;
 
         _actions.AddAction(uid, ref component.InjectActionEntity, component.InjectActionId);
-    }
-
-    private void OnEmagged(EntityUid uid, EmaggableMedibotComponent comp, ref GotEmaggedEvent args)
-    {
-        if (!TryComp<MedibotComponent>(uid, out var medibot))
-            return;
-
-        _audio.PlayPredicted(comp.SparkSound, uid, args.UserUid);
-
-        foreach (var (state, treatment) in comp.Replacements)
-            medibot.Treatments[state] = treatment;
-
-        args.Handled = true;
     }
 
     private void OnInjectTarget(ref MedibotInjectTargetEvent ev)
--- Content.Shared/Silicons/Bots/MedibotSystem.cs
+++ Content.Shared/Silicons/Bots/MedibotSystem.cs
@@ -1,4 +1,6 @@
+using Content.Shared.Emag.Systems;
 using Content.Shared.Mobs;
+using Robust.Shared.Audio.Systems;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Content.Shared.Silicons.Bots;
@@ -8,6 +10,28 @@ namespace Content.Shared.Silicons.Bots;
 /// </summary>
 public abstract class SharedMedibotSystem : EntitySystem
 {
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<EmaggableMedibotComponent, GotEmaggedEvent>(OnEmagged);
+    }
+
+    private void OnEmagged(EntityUid uid, EmaggableMedibotComponent comp, ref GotEmaggedEvent args)
+    {
+        if (!TryComp<MedibotComponent>(uid, out var medibot))
+            return;
+
+        _audio.PlayPredicted(comp.SparkSound, uid, args.UserUid);
+
+        foreach (var (state, treatment) in comp.Replacements)
+            medibot.Treatments[state] = treatment;
+
+        args.Handled = true;
+    }
+
     /// <summary>
     /// Get a treatment for a given mob state.
     /// </summary>
