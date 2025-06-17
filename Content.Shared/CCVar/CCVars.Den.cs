cc6c47749728879e9537f66faccbb7fa1194d4ab
sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>
2025-06-17 08:00:54 -0300

    Update CCVars.Den.cs

--- Content.Shared/CCVar/CCVars.Den.cs
+++ Content.Shared/CCVar/CCVars.Den.cs
@@ -9,5 +9,5 @@ public sealed partial class CCVars
     ///     How many Low Danger rounds should there be after a High Danger round?
     /// </summary>
     public static readonly CVarDef<int> LowDangerRoundsAfterHighDanger =
-        CVarDef.Create("game.low_danger_rounds_after_high_danger", 2, CVar.SERVER | CVar.ARCHIVE);
+        CVarDef.Create("game.low_danger_rounds_after_high_danger", 1, CVar.SERVER | CVar.ARCHIVE);
 }
