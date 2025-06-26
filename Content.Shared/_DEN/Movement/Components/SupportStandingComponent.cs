d8e7efdbada3cd4ea70e894e61b903e52707e225
portfiend <109661617+portfiend@users.noreply.github.com>
2025-06-26 16:35:11 -0400

    refactor: supportStanding abstract component

--- Content.Shared/_DEN/Movement/Components/SupportStandingComponent.cs
+++ Content.Shared/_DEN/Movement/Components/SupportStandingComponent.cs
@@ -1,27 +1,28 @@
 namespace Content.Shared._DEN.Movement.Components;
 
 /// <summary>
-///     A component given to items that allow characters with missing legs to stand up instead of being forced to crawl,
+///     A component given to items that allow characters with missing legs to stand up instead of being forced to crawl.
 /// </summary>
 
-[RegisterComponent]
-public sealed partial class SupportStandingComponent : Component
+public abstract partial class SupportStandingComponent : Component
 {
     /// <summary>
     ///     The minimum number of legs needed in order to provide standing support.
     /// </summary>
     [DataField]
     public int MinimumLegCount = 1;
-
-    /// <summary>
-    ///     Whether this item provides standing support when held in the hand.
-    /// </summary>
-    [DataField]
-    public bool WorksInHand = false;
-
-    /// <summary>
-    ///     Whether this item provides standing support when worn.
-    /// </summary>
-    [DataField]
-    public bool WorksWhenEquipped = true;
 }
+
+/// <summary>
+///     Allows characters with missing legs to stand up while holding this item.
+/// </summary>
+[RegisterComponent]
+public sealed partial class HeldSupportStandingComponent : SupportStandingComponent
+{ }
+
+/// <summary>
+///     Allows characters with missing legs to stand up while wearing this item.
+/// </summary>
+[RegisterComponent]
+public sealed partial class WornSupportStandingComponent : SupportStandingComponent
+{ }
--- Content.Shared/_DEN/Movement/Systems/SharedSupportStandingSystem.cs
+++ Content.Shared/_DEN/Movement/Systems/SharedSupportStandingSystem.cs
@@ -1,31 +1,57 @@
 using Content.Shared._DEN.Body;
 using Content.Shared._DEN.Movement.Components;
+using Content.Shared.Body.Components;
 using Content.Shared.Hands;
 using Content.Shared.Inventory;
+using Content.Shared.Inventory.Events;
+using Content.Shared.Standing;
 
 namespace Content.Shared._DEN.Movement.Systems;
 
 public abstract class SharedSupportStandingSystem : EntitySystem
 {
+    [Dependency] private readonly StandingStateSystem _standing = default!;
+
     public override void Initialize()
     {
-        SubscribeLocalEvent<SupportStandingComponent,
+        SubscribeLocalEvent<HeldSupportStandingComponent, GotUnequippedHandEvent>(OnGotUnequippedHand);
+        SubscribeLocalEvent<WornSupportStandingComponent, GotUnequippedEvent>(OnGotUnequipped);
+
+        SubscribeLocalEvent<HeldSupportStandingComponent,
+            HeldRelayedEvent<CannotSupportStandingEvent>>(SupportStandingWhenHeld);
+        SubscribeLocalEvent<WornSupportStandingComponent,
             InventoryRelayedEvent<CannotSupportStandingEvent>>(SupportStandingWhenWorn);
-        SubscribeLocalEvent<SupportStandingComponent,
-            HeldRelayedEvent<CannotSupportStandingEvent>>(SupportStandingInHands);
     }
 
-    protected void SupportStandingInHands(Entity<SupportStandingComponent> ent,
+    private void OnGotUnequippedHand(Entity<HeldSupportStandingComponent> ent, ref GotUnequippedHandEvent args)
+        => UpdateStanding(args.User);
+
+    private void OnGotUnequipped(Entity<WornSupportStandingComponent> ent, ref GotUnequippedEvent args)
+        => UpdateStanding(args.Equipee);
+
+    protected void SupportStandingWhenHeld(Entity<HeldSupportStandingComponent> ent,
         ref HeldRelayedEvent<CannotSupportStandingEvent> args)
+        => TrySupportStanding(ent.Comp, ref args.Args);
+
+    protected void SupportStandingWhenWorn(Entity<WornSupportStandingComponent> ent,
+        ref InventoryRelayedEvent<CannotSupportStandingEvent> args)
+        => TrySupportStanding(ent.Comp, ref args.Args);
+
+    private void UpdateStanding(EntityUid uid)
     {
-        if (args.Args.LegCount >= ent.Comp.MinimumLegCount && ent.Comp.WorksInHand)
-            args.Args.Cancel();
+        if (!TryComp<BodyComponent>(uid, out var body))
+            return;
+
+        var ev = new CannotSupportStandingEvent(body.LegEntities.Count);
+        RaiseLocalEvent(uid, ev);
+
+        if (!ev.Cancelled)
+            _standing.Down(uid);
     }
 
-    protected void SupportStandingWhenWorn(Entity<SupportStandingComponent> ent,
-        ref InventoryRelayedEvent<CannotSupportStandingEvent> args)
+    private void TrySupportStanding(SupportStandingComponent comp, ref CannotSupportStandingEvent args)
     {
-        if (args.Args.LegCount >= ent.Comp.MinimumLegCount && ent.Comp.WorksWhenEquipped)
-            args.Args.Cancel();
+        if (args.LegCount >= comp.MinimumLegCount)
+            args.Cancel();
     }
 }
