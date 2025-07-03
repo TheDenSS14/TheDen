34687ace5de5b957c29e905875c22f1c212b9fba
Memeji Dankiri <39416235+Memeji@users.noreply.github.com>
2025-07-03 12:37:37 -0500

    Merge pull request #1165 from Cprn-Sprkl/Marked-Traits
    Marked Traits

--- Content.Server/Objectives/Components/PickRandomPersonComponent.cs
+++ Content.Server/Objectives/Components/PickRandomPersonComponent.cs
@@ -16,4 +16,19 @@ public sealed partial class PickRandomPersonComponent : Component
 {
     [DataField]
     public bool NeedsOrganic; // Goobstation: Only pick non-silicon players.
+    
+    //Floofstation Target Consent Traits: Start
+    [DataField]
+    public ObjectiveTypes ObjectiveType;
+    //Floofstation Target Consent Traits: End
 }
+
+//Floofstation Target Consent Traits: Start
+[Flags]
+public enum ObjectiveTypes
+{
+    Unspecified = 0,
+    TraitorKill = 1 << 0,
+    TraitorTeach = 1 << 1
+}
+//Floofstation Target Consent Traits: End
---
+++ Resources/Locale/en-US/_Floof/traits/traits.ftl
@@ -0,0 +1,7 @@
+# Target Consent Traits: Start
+trait-name-TraitorKillTarget = Marked for Removal
+trait-description-TraitorKillTarget = Someone or something wants you REMOVED. Traitors can recieve orders to kill you and keep you dead.
+
+trait-name-TraitorTeachTarget = Marked for Death
+trait-description-TraitorTeachTarget = Someone or something wants you taught a lesson. Traitors can recieve orders to kill you.
+# Target Consent Traits: End
