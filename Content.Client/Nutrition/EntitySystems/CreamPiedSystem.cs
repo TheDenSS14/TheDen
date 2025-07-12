281b9bfeee9e5542bfd7fe59408c9a614953723b
portfiend <109661617+portfiend@users.noreply.github.com>
2025-07-12 11:48:54 -0400

    add: creampied sprite state initializes itself

--- Content.Client/Nutrition/EntitySystems/CreamPiedSystem.cs
+++ Content.Client/Nutrition/EntitySystems/CreamPiedSystem.cs
@@ -4,13 +4,38 @@
 //
 // SPDX-License-Identifier: MIT
 
+using Content.Shared.Nutrition.Components;
 using Content.Shared.Nutrition.EntitySystems;
 using JetBrains.Annotations;
+using Robust.Client.GameObjects;
 
-namespace Content.Client.Nutrition.EntitySystems
+namespace Content.Client.Nutrition.EntitySystems;
+
+[UsedImplicitly]
+public sealed class CreamPiedSystem : SharedCreamPieSystem
 {
-    [UsedImplicitly]
-    public sealed class CreamPiedSystem : SharedCreamPieSystem
+    [Dependency] private readonly SpriteSystem _spriteSystem = default!;
+
+    public override void Initialize()
     {
+        base.Initialize();
+
+        SubscribeLocalEvent<CreamPiedComponent, ComponentStartup>(OnComponentStartup);
     }
+
+    // DEN change: Initialize cream pie sprites here so we don't have to change sprite layer orders jfc
+    private void OnComponentStartup(Entity<CreamPiedComponent> ent, ref ComponentStartup args)
+    {
+        UpdateCreamPiedSprite(ent);
+    }
+
+    private void UpdateCreamPiedSprite(Entity<CreamPiedComponent> ent)
+    {
+        if (ent.Comp.Layer == null
+            || !TryComp<SpriteComponent>(ent.Owner, out var sprite))
+            return;
+
+        _spriteSystem.LayerSetData((ent.Owner, sprite), ent.Comp.LayerKey, ent.Comp.Layer);
+    }
+    // End DEN
 }
--- Content.Shared/Nutrition/Components/CreamPiedComponent.cs
+++ Content.Shared/Nutrition/Components/CreamPiedComponent.cs
@@ -18,6 +18,20 @@ namespace Content.Shared.Nutrition.Components
     {
         [ViewVariables]
         public bool CreamPied { get; set; } = false;
+
+        /// <summary>
+        ///     DEN: When CreamPiedComponent is activated, their creampied sprite visuals are replaced with this.
+        ///     Make sure not to put in a `map` or `visible` value here, or it is bound to break something.
+        ///     Alternatively, you can ignore this if you're already defining the sprite in layer order.
+        /// </summary>
+        [DataField]
+        public PrototypeLayerData? Layer;
+
+        /// <summary>
+        ///     This is the sprite map key that should be replaced when this CreamPiedComponent activates.
+        /// </summary>
+        [DataField]
+        public string LayerKey = "clownedon";
     }
 
     [Serializable, NetSerializable]
