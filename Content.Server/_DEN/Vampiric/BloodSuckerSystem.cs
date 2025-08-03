1ebdca8066ef97ba5e955be9450bc9300a8c93f9
sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>
2025-08-03 10:17:39 -0300

    Update Content.Server/_DEN/Vampiric/BloodSuckerSystem.cs

--- Content.Server/_DEN/Vampiric/BloodSuckerSystem.cs
+++ Content.Server/_DEN/Vampiric/BloodSuckerSystem.cs
@@ -12,9 +12,7 @@ public sealed partial class BloodSuckerSystem
     private void OnStartup(Entity<BloodSuckerComponent> ent, ref ComponentStartup args)
     {
         if (!HasComp<BloodExaminerComponent>(ent))
-        {
             ent.Comp.AddedBloodExaminer = EnsureComp<BloodExaminerComponent>(ent);
-        }
     }
 
     private void OnShutdown(Entity<BloodSuckerComponent> ent, ref ComponentShutdown args)
