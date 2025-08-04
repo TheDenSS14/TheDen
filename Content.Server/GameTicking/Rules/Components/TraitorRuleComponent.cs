9bb56c53a32af851e774766704fd45c03a4e62e0
Jakumba <16194688+Jakumba@users.noreply.github.com>
2025-08-04 01:48:17 +0100

    Reset TC provided to antags & reset cost changes

--- Content.Server/GameTicking/Rules/Components/TraitorRuleComponent.cs
+++ Content.Server/GameTicking/Rules/Components/TraitorRuleComponent.cs
@@ -105,5 +105,5 @@ public sealed partial class TraitorRuleComponent : Component
     /// The amount of TC traitors start with.
     /// </summary>
     [DataField]
-    public int StartingBalance = 175;
+    public int StartingBalance = 100;
 }
