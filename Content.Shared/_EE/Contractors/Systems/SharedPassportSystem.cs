8ca8b6a0795ac1c8b9a02e205cf858ab57a6a275
sev7ves <soapedzone@gmail.com>
2025-08-09 18:49:29 -0400

    made it not hard coded

--- Content.Shared/_EE/Contractors/Systems/SharedPassportSystem.cs
+++ Content.Shared/_EE/Contractors/Systems/SharedPassportSystem.cs
@@ -73,9 +73,6 @@ public class SharedPassportSystem : EntitySystem
         if (Deleted(ev.Mob) || !Exists(ev.Mob))
             return;
 
-        if(ev.JobId=="StationAi" || ev.JobId=="Borg")//den edit
-            return;
-
         if (!_prototypeManager.TryIndex(
             ev.Profile.Nationality,
             out NationalityPrototype? nationalityPrototype) || !_prototypeManager.TryIndex(nationalityPrototype.PassportPrototype, out EntityPrototype? entityPrototype))
