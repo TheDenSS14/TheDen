ced460ea8e84388294054a36a45125ac53c9b14e
sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>
2025-08-15 02:25:30 -0300

    What the hell.

--- Content.Shared/_CD/Records/PlayerProvidedCharacterRecords.cs
+++ Content.Shared/_CD/Records/PlayerProvidedCharacterRecords.cs
@@ -173,10 +173,12 @@ public sealed partial class PlayerProvidedCharacterRecords
 
     private static string ClampString(string str, int maxLen)
     {
+        if (string.IsNullOrWhiteSpace(str))
+            return string.Empty;
+
         if (str.Length > maxLen)
-        {
             return str[..maxLen];
-        }
+
         return str;
     }
 
