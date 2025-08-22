208ea7b55c8a284b7e4d8ed95332f069e22c7680
sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>
2025-08-21 21:58:10 -0300

    woof bark

---
+++ Content.Server/_DEN/Discord/DiscordLink/InGameCommands.cs
@@ -0,0 +1,14 @@
+namespace Content.Server._DEN.Discord.DiscordLink;
+
+
+/// <summary>
+/// This handles discord commands that call game code.
+/// </summary>
+public sealed class InGameCommands : EntitySystem
+{
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<On();
+    }
+}
