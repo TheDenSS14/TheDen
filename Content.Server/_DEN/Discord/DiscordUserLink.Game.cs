a762c469d408e53c8d17522d4a09235501a612fd
sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>
2025-08-22 21:33:24 -0300

    !

--- Content.Server/_DEN/Discord/DiscordUserLink.Game.cs
+++ Content.Server/_DEN/Discord/DiscordUserLink.Game.cs
@@ -15,12 +15,12 @@ public sealed partial class DiscordUserLink
 {
     public void InitializeGame() {}
 
-    private void OnPlayerStatusChanged(object? sender, SessionStatusEventArgs ev)
+    private async void OnPlayerStatusChanged(object? sender, SessionStatusEventArgs ev)
     {
         if (ev.NewStatus != SessionStatus.Connected)
             return;
 
-        Task.Run(async () => await SetupPlayerAsync(ev.Session.UserId));
+        await SetupPlayerAsync(ev.Session.UserId);
     }
 
     private async Task SetupPlayerAsync(NetUserId userId)
