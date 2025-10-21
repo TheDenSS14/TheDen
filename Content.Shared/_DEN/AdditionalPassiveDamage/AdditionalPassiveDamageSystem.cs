e880b588799ce32846aab4a231fe1f28434878a6
ASaltySeabass <SKKJMK1@gmail.com>
2025-10-20 21:19:37 -0400

    Update Content.Shared/_DEN/AdditionalPassiveDamage/AdditionalPassiveDamageSystem.cs
    Co-authored-by: sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>

--- Content.Shared/_DEN/AdditionalPassiveDamage/AdditionalPassiveDamageSystem.cs
+++ Content.Shared/_DEN/AdditionalPassiveDamage/AdditionalPassiveDamageSystem.cs
@@ -54,7 +54,7 @@ public sealed class AdditionalPassiveDamage : EntitySystem
             // Damage them
             foreach (var allowedState in comp.AllowedStates)
             {
-                if(allowedState == mobState.CurrentState)
+                if (allowedState == mobState.CurrentState)
                     _damageable.TryChangeDamage(uid, comp.Damage, true, false, damage);
             }
         }
