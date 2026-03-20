6e46be119e1892e981496b90100826581b7c377f
portfiend <109661617+portfiend@users.noreply.github.com>
2026-03-20 16:22:57 -0500

    add simple prefix-suffix accent

---
+++ Content.Server/_DEN/Speech/Components/PrefixSuffixAccentComponent.cs
@@ -0,0 +1,32 @@
+namespace Content.Server._DEN.Speech.Components;
+
+/// <summary>
+///     A component that automatically adds text to the start and end of your dialogue.
+///     Good for admemes, especially formatting tags.
+/// </summary>
+/// <remarks>
+///     This component is actually a misnomer. Prefix-suffix can be applied *after* accents are
+///     processed, so the prefix and suffix doesn't get affected by accent.
+/// </remarks>
+[RegisterComponent]
+public sealed partial class PrefixSuffixAccentComponent : Component
+{
+    /// <summary>
+    ///     Text to add to the start of a dialogue message.
+    /// </summary>
+    [DataField] public string Prefix = string.Empty;
+
+    /// <summary>
+    ///     Text to add to the end of a dialogue message.
+    /// </summary>
+    [DataField] public string Suffix = string.Empty;
+
+    /// <summary>
+    ///     Whether or not this accent should be processed alongside other accents,
+    ///     or if it should happen afterwards (preventing your prefix-suffix from being rearranged.)
+    /// </summary>
+    /// <remarks>
+    ///     Keep this "false" if you're using formatting tags!
+    /// </remarks>
+    [DataField] public bool AffectedByAccents = false;
+}
