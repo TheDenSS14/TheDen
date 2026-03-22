16e96668cccbf49ac81231936eda9c4fb263b30e
Jakumba <16194688+Jakumba@users.noreply.github.com>
2026-03-22 19:16:42 +0000

    Revert "try to unzombify after tumor removal"
    This reverts commit 75d9e7c5b98ea3a1eb2cda0d3b406891de95439d.

--- Content.Server/Zombies/ZombieTumorOrganSystem.cs
+++ Content.Server/Zombies/ZombieTumorOrganSystem.cs
@@ -953,12 +953,6 @@ public sealed class ZombieTumorOrganSystem : SharedZombieTumorOrganSystem
         if (TerminatingOrDeleted(ent.Owner) || args.OldBody == EntityUid.Invalid || TerminatingOrDeleted(args.OldBody))
             return;
 
-        // Den - if the target is already a zombie, revert if possible
-        if (TryComp<ZombieComponent>(ent.Owner, out var zomComp))
-        {
-            _zombieSystem.UnZombify(ent.Owner, ent.Owner, zomComp);
-        }
-
         // When tumor is removed, cure the infection
         if (HasComp<ZombieTumorInfectionComponent>(args.OldBody))
         {
@@ -967,7 +961,6 @@ public sealed class ZombieTumorOrganSystem : SharedZombieTumorOrganSystem
             var removalMessage = wasRoboTumor ? "zombie-robotumor-removed" : "zombie-tumor-removed";
 
             RemComp<ZombieTumorInfectionComponent>(args.OldBody);
-
             _popup.PopupEntity(Loc.GetString(removalMessage), args.OldBody, args.OldBody);
         }
     }
