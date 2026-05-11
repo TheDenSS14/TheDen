ece3cd50fb4468a039ca4628ec1745676f8e17c1
MajorMoth <61519600+MajorMoth@users.noreply.github.com>
2026-05-11 09:22:57 +0200

    whoops

--- Content.Shared/Overlays/BaseEquipmentHudComponent.cs
+++ Content.Shared/Overlays/BaseEquipmentHudComponent.cs
@@ -6,12 +6,6 @@ namespace Content.Shared.Overlays;
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public abstract partial class BaseEquipmentHudComponent : Component
 {
-    /// <summary>
-    ///     Action used to control which elements of the HUD to show.
-    /// </summary>
-    [DataField, AutoNetworkedField]
-    public EntProtoId Action = "ActionControlHud";
-
     [DataField, AutoNetworkedField]
     public EntityUid? ActionEntity;
 }
